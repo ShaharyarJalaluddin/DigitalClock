cb7c9ff291b8b92a156ce318c0415db8ae75cea3
Shaharyar Jalaluddin <46268083+ShaharyarJalaluddin@users.noreply.github.com>
2020-04-04 20:29:16 +0500

    Add files via upload

---
+++ function.js
@@ -0,0 +1,22 @@
+function Clock(){
+    const fullDate = new Date()
+    let hours = fullDate.getHours()
+    let mins = fullDate.getMinutes()
+    let secs = fullDate.getSeconds()
+
+    if(hours<10){
+        hours = '0' + hours
+    }
+    if(mins<10){
+        mins = '0' + mins
+    }
+    if(secs<10){
+        secs = '0' + secs
+    }
+
+    document.getElementById('hour').innerHTML = hours + ':'
+    document.getElementById('minutes').innerHTML = mins + ':'
+    document.getElementById('secs').innerHTML = secs
+}
+
+setInterval(Clock, 100)
